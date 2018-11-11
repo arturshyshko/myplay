7fcec86a897bf6c8aa6ed7ab54bd1433182e6aea
oleh <lovemybeard@gmail.com>
2018-11-11 18:07:39 +0200

    serve client app from nginx

--- server/myplay/myplay/settings.py
+++ server/myplay/myplay/settings.py
@@ -129,5 +129,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/djangostatic/'
 STATIC_ROOT = os.path.join(BASE_DIR, '.build', 'static')
