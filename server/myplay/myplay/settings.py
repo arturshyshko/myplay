38d0646aeb347a5c3294bd335e1cd1fcf4227435
oleh <lovemybeard@gmail.com>
2018-11-14 12:13:29 +0200

    add django logging

--- server/myplay/myplay/settings.py
+++ server/myplay/myplay/settings.py
@@ -131,3 +131,27 @@ USE_TZ = True
 
 STATIC_URL = '/djangostatic/'
 STATIC_ROOT = os.path.join(BASE_DIR, '.build', 'static')
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'console': {
+            # exact format is not important, this is the minimum information
+            'format': '%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
+        },
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'console',
+        },
+    },
+    'loggers': {
+        # root logger
+        '': {
+            'level': os.environ.get('LOG_LEVEL', 'info'),
+            'handlers': ['console'],
+        },
+    },
+}
